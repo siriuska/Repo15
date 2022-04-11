23ba04126ba551d04e0fbd26295b35f5db980274
siriuska <you@example.com>
2022-04-11 22:01:46 +0200

    The rest parameter with function parameters

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const product = (...args) => {
+    return args.reduce((a, b) => a * b, 1);
+}
+console.log(product(1, 2));
+console.log(product(4, 3, 10, 2));
+console.log(product(7));
+console.log(product());
+module.exports = product;
